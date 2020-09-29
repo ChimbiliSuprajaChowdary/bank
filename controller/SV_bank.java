9b62d5c6ef595da47606f4a698f64edb0598ff15
vamshiduddu <44239287+vamshiduddu@users.noreply.github.com>
2020-09-29 15:40:18 +0530

    admin part left

--- controller/SV_bank.java
+++ controller/SV_bank.java
@@ -17,7 +17,9 @@ public class SV_bank {
 		
 		Bank_services service=new Bank_services();
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
+		boolean success = false;
+		while (!success) {
+		try {
 		do {
 			System.out.println("WELCOME TO THE BANK");
 			System.out.println("Enter your choice");
@@ -25,103 +27,196 @@ public class SV_bank {
 			System.out.println("2.User Login");
 			System.out.println("3.Apply for bank account");
 			int choice = Integer.parseInt(br.readLine());
-			switch(choice) {
-			case 1: 
-			{
-				Admin admin=new Admin();
-				String adminName;
-				String password;
-				password=br.readLine();
-				System.out.println("Enter your username");
-				adminName=br.readLine();
-				System.out.println("Enter your password");
-				password=br.readLine();
-				if(adminName.contentEquals(admin.getUserName()) && password.contentEquals(admin.getPassword()))
+			
+				
+				switch(choice) {
+				case 1: 
 				{
-					System.out.println("welcome admin");
-					do
+					Admin admin=new Admin();
+					String adminName;
+					String password;
+					password=br.readLine();
+					System.out.println("Enter your username");
+					adminName=br.readLine();
+					System.out.println("Enter your password");
+					password=br.readLine();
+					if(adminName.contentEquals(admin.getUserName()) && password.contentEquals(admin.getPassword()))
 					{
-						System.out.println("\n1.Get user details"
-								+ "\n3.Update user details/n4.Delete user details");
-						int operation=Integer.parseInt(br.readLine());
-						switch(operation)
+						System.out.println("welcome admin");
+						do
 						{
-						case 1:
-						{}
-						case 2:
-						{}
-						case 3:
-						{}
-						case 4:
-						{}
-						default:
-						{
-							System.out.println("Access denied!!");
-							break;
-						}
-						}
+							System.out.println("\n1.Get user details"
+									+ "\n3.Update user details/n4.Delete user details");
+							int operation=Integer.parseInt(br.readLine());
+							switch(operation)
+							{
+							case 1:
+							{}
+							case 2:
+							{}
+							case 3:
+							{}
+							case 4:
+							{}
+							default:
+							{
+								System.out.println("Access denied!!");
+								break;
+							}
+							}
+						
+						}while(true);
 					
-					}while(true);
-				
-			}
 				}
-			case 2:
-			{
-				
-					System.out.println("welcome user");
-					do
-					{
-						System.out.println("\n1.Deposit money\n2.Withdraw money"
-								+ "\n3.Apply for loan/n4.Apply for .....");
-						int operation=Integer.parseInt(br.readLine());
-						switch(operation)
-						{
-						case 1:
-						{}
-						case 2:
-						{}
-						case 3:
-						{}
-						case 4:
-						{}
-						default:
-						{
-							System.out.println("choose correct option");
-							break;
-						}
-						}
+					}
+				case 2:
+				{
+					User testUser = null;
 					
-					}while(true);
+					String userName;
+					String userpassword;
+					//user.setUserName(userName);
+					
+					System.out.println("Enter your username");
+					userName=br.readLine();
 				
-			}
-			case 3:
-			{
-				System.out.println("Fill the application:");
+					System.out.println("Enter your password");
+					userpassword=br.readLine();
+					
+					//testUser.setUserName(userName);
+					//testUser.setPassword1(userpassword);
+					boolean isBreakTheLoop = false;
+					for(int i=0;i<usersList.size();i++)
+					{
+						testUser=usersList.get(i);
+						if(testUser.getUserName().contentEquals(userName) && testUser.getPassword1().contentEquals(userpassword))
+						{
+							System.out.println("welcome "+userName);
+							do
+							{
+								System.out.println("\n1.Deposit money\n2.Withdraw money"
+										+ "\n3.Apply for loan \n4.Apply for Credit Card \n5.Get Current balance \n6.Logout");
+								int operation=Integer.parseInt(br.readLine());
+								switch(operation)
+								{
+								case 1:
+								{
+									isBreakTheLoop=false;
+									System.out.println("Enter your amount :");
+									long deposit= Long.parseLong(br.readLine());
+									deposit = service.deposit(testUser, deposit);
+									System.out.println("Your updated balance:"+testUser.getBankBalance());
+									break;
+								}
+								case 2:
+								{
+									isBreakTheLoop=false;
+									System.out.println("Enter your Withdraw :");
+									long withdraw= Long.parseLong(br.readLine());
+									String res = service.withDraw(testUser, withdraw);
+									
+									System.out.println(res);
+									System.out.println("Your current Balence "+testUser.getBankBalance());
+									break;
+									
+									
+								}
+								case 3:
+								{
+									isBreakTheLoop=false;
+									String loanString = service.applyLoan(testUser);
+									System.out.println(loanString);
+									System.out.println("Your current Balence "+testUser.getBankBalance());
+									break;
+								}
+								case 4:
+								{
+									isBreakTheLoop=false;
+									String creString = service.applyCredit(testUser);
+									System.out.println(creString);
+									System.out.println("Your current Balence "+testUser.getBankBalance());6
+									
+									break;
+								}
+								case 5:
+								{
+									isBreakTheLoop=false;
+									System.out.println("Your current Balence "+testUser.getBankBalance());
+									break;
+									
+								}
+								case 6:
+									isBreakTheLoop=true;
+									System.out.println("Get OUT");
+									break;
+									
+								default:
+								{
+									System.out.println("choose correct option");
+									break;
+								}
+								}
+							
+							}while(!isBreakTheLoop);
+						}
+						else {
+							System.out.println("Failed ..!!");
+						}
+					}
+					
+					
+						
+					
+					
+					
+				}
+				break;
+				case 3:
+				{
+					System.out.println("Fill the application:");
+					System.out.println("Enter YourName:");
+					String userName=br.readLine();
+					System.out.println("Create a Password:");
+					String password1=br.readLine();;
+					System.out.println("Your age:");
+					int age=Integer.parseInt(br.readLine());
+					System.out.println("Enter Your Mobile number");
+					String phoneNumber=br.readLine();
+					System.out.println("Enter Your Address");
+					String address=br.readLine();
+					System.out.println("Any proof ID - with Proof Name");
+					String proofId=br.readLine();
+					System.out.println("How much do you want deposit ?");
+					long bankBalance=Long.parseLong(br.readLine());
+					
+					User user=new User();
+					
+					user.setUserName(userName);
+					user.setPassword1(password1);
+					user.setAge(age);
+					user.setPhoneNumber(phoneNumber);
+					user.setAddress(address);
+					user.setProofId(proofId);
+					user.setBankBalance(bankBalance);
+					//admin validation
+					usersList=service.addUser(user);
+					System.out.println("user added");
+					
+				}
+				}
 				
-				String userName=br.readLine();
-				String password1=br.readLine();;
-				int age=Integer.parseInt(br.readLine());
-				String phoneNumber=br.readLine();
-				String address=br.readLine();
-				String proofId=br.readLine();
-				long bankBalance=Long.parseLong(br.readLine());
-				
-				User user=new User();
-				
-				user.setUserName(userName);
-				user.setPassword1(password1);
-				user.setAge(age);
-				user.setPhoneNumber(phoneNumber);
-				user.setAddress(address);
-				user.setProofId(proofId);
-				user.setBankBalance(bankBalance);
-				//admin validation
-				usersList=service.addUser(user);
-				System.out.println("user added");
-				
-			}
-			}
 			}while(true);
+		}
+			
+				
+			 catch (Exception e) {
+				System.out.println("Input only Integers please");
+			}
+			
+			}
+			
+		
+		
 
 		}
 }
--- service/Bank_services.java
+++ service/Bank_services.java
@@ -3,16 +3,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.User;
+import sun.security.x509.AVA;
 public class Bank_services
 {
-	static List<User> users=new ArrayList<User>();
-	public List addUser(User user)
-	{
-		users.add(user);
-		System.out.println("Adding user");
-		return users;
+		static List<User> users=new ArrayList<User>();
+		public List<User> addUser(User user)
+		{
+			users.add(user);
+			System.out.println("Adding user");
+			return users;
+			
+		}
+		public long deposit(User user,long deposit) {
+			
+			long avblBalance = user.getBankBalance();
+			user.setBankBalance(avblBalance+deposit);
+			return avblBalance+deposit;
+		}
 		
-	}
+		public String withDraw(User user,long withdraw) {
+				
+				long avblBalance = user.getBankBalance();
+				if(avblBalance<withdraw)
+				{
+					return "You don't have that much money man";
+				}
+				else {
+					user.setBankBalance(avblBalance-withdraw);
+					return "Updated balance "+String.valueOf(avblBalance-withdraw);
+				}
+				
+			}
+		
+		public String applyLoan(User user) {
+			long avblBalance = user.getBankBalance();
+			if(avblBalance>=5000)
+			{
+				return "Loan has Been applied.!";
+				
+			}
+			else {
+				return "Minimum Balance to apply loan is 5000";
+			}
+			
+		}
+		public String applyCredit(User user) {
+			long avblBalance = user.getBankBalance();
+			if(avblBalance>=10000)
+			{
+				return "Credit card has Been applied.!";
+				
+			}
+			else {
+				return "Minimum Balance to apply Credit card is 10000";
+			}
+			
+		}
+	
 	
 	
 }
